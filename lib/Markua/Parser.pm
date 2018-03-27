6605937fce85337d0b9aa7853611ea0edfe1ec68
Gabor Szabo <gabor@szabgab.com>
2018-03-27 18:52:00 +0300

    implement first version of resource parsing

--- lib/Markua/Parser.pm
+++ lib/Markua/Parser.pm
@@ -13,19 +13,43 @@ sub new {
 
 sub parse_file {
     my ($self, $filename) = @_;
+    my $path = path($filename);
+    my $dir = $path->parent->stringify;
     my @entries;
     my @errors;
     my $cnt = 0;
 
     $self->{text} = '';
 
-    for my $line (path($filename)->lines_utf8) {
+    for my $line ($path->lines_utf8) {
         $cnt++;
         if ($line =~ /^(#{1,6}) (\S.*)/) {
             push @entries, {
                 tag => 'h' . length($1),
                 text => $2,
             };
+            next;
+        }
+
+# I should remember to always use \A instead of ^ even thoygh here we are really parsing lines so those two are the same
+        if ($line =~ /\A ! \[([^\]]*)\]    \(([^\)]+)\)  \s* \Z/x) {
+            my $title = $1;
+            my $file_to_include = $2;
+            eval {
+                my $text = path("$dir/$file_to_include")->slurp_utf8;
+                push @entries, {
+                    tag   => 'code',
+                    title => $title,
+                    text  => $text,
+                };
+            };
+            if ($@) {
+                push @errors, {
+                    row => $cnt,
+                    line => $line,
+                    error => "Could not read included file '$file_to_include'",
+                };
+            }
             next;
         }
 
--- t/01-test.t
+++ t/01-test.t
@@ -6,7 +6,7 @@ use JSON::MaybeXS qw(decode_json);
 use Path::Tiny qw(path);
 use Markua::Parser;
 
-my @cases = ('heading1', 'headers', 'paragraphs');
+my @cases = ('heading1', 'headers', 'paragraphs', 'include');
 
 plan tests => 1 + 2 * scalar @cases;
 
@@ -15,7 +15,12 @@ isa_ok $m, 'Markua::Parser';
 
 for my $case (@cases) {
     my ($result, $errors) = $m->parse_file("t/input/$case.md");
-    is_deeply $result, decode_json( path("t/dom/$case.json")->slurp_utf8 );
-    is_deeply $errors, [], "Errors of $case" or diag explain $errors;
+    is_deeply $result, decode_json( path("t/dom/$case.json")->slurp_utf8 ) or diag explain $result;
+    my $expected_errors = [];
+    my $error_path = path("t/errors/$case.json");
+    if ($error_path->exists) {
+        $expected_errors = decode_json( $error_path->slurp_utf8 );
+    }
+    is_deeply $errors, $expected_errors, "Errors of $case" or diag explain $errors;
 }
 
